7cafa60b75cfa89d5f3200797c687a4f1b2f0383
Brendan Brake <bab1007@jagmail.southalabama.edu>
2018-09-19 13:45:23 -0500

    Initial Commit with Default ASP.NET Core folder structure. No Authentication present. 9_19_2018

---
+++ The_Professionals_SeniorProject/The_Professionals_SeniorProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace The_Professionals_SeniorProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
