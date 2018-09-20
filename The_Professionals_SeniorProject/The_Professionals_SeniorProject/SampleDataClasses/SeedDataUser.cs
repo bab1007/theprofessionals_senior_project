0bcfd2bf37a49354fd2db7d05d419f619f0fb5fd
Brendan Brake <bab1007@jagmail.southalabama.edu>
2018-09-19 23:22:00 -0500

    Added SampleDataClass and removed repository so that entityframework will spawn sample data for Users on Add-Migration. This will allow any of us to be able to "spawn" a database with sample data already in place using a fresh build of the project. Sample data inside of the SeedDataUser.cs class still needs to be added, will be done later.

---
+++ The_Professionals_SeniorProject/The_Professionals_SeniorProject/SampleDataClasses/SeedDataUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using The_Professionals_SeniorProject.Models.Schema;
+using The_Professionals_SeniorProject.DAL;
+
+namespace The_Professionals_SeniorProject.SampleDataClasses
+    //ToDo: Create sample data for users
+
+{
+//    public static class SeedDataUser
+//    {
+//       public static void EnsurePopulated(IApplicationBuilder app)
+//        {
+//            AchievementContext context = app.ApplicationServices.GetRequiredService<AchievementContext>();
+//            if (!context.Users.Any())
+//            {
+//                context.Users.AddRange{
+//                    new User
+//                    {
+//                        Fname = 
+//                    }
+
+//                }
+//            }
+//        }
+
+
+//    }
+}
